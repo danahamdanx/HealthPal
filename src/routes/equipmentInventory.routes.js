2a9c25cd8e049fd77acf99c84ef6caf8482002c2
danahamdanx <dana29454@gmail.com>
2025-11-29 19:46:59 +0200

    correct the equipment inventory

--- src/routes/equipmentInventory.routes.js
+++ src/routes/equipmentInventory.routes.js
@@ -8,13 +8,19 @@ import {
   deleteEquipment
 } from "../controllers/equipmentInventory.controller.js";
 import { authorizeRoles } from '../middleware/authorize.middleware.js';
+import { authenticate } from "../middleware/authenticate.js";
 
 const router = express.Router();
 
-router.post("/", authorizeRoles, createEquipment);
-router.get("/", authorizeRoles, getAllEquipment);
-router.get("/:id", authorizeRoles, getEquipmentById);
-router.put("/:id", authorizeRoles, updateEquipment);
-router.delete("/:id", authorizeRoles, deleteEquipment);
+router.post("/",authenticate, authorizeRoles("admin","ngo","donor"), createEquipment);
+
+router.get("/",authenticate, authorizeRoles("admin","patient","ngo","donor"), getAllEquipment);
+
+router.get("/:id",authenticate, authorizeRoles("admin","ngo","donor","patient"), getEquipmentById);
+
+router.put("/:id",authenticate, authorizeRoles("admin","ngo","donor"), updateEquipment);
+
+router.delete("/:id",authenticate, authorizeRoles("admin","ngo","donor"), deleteEquipment);
+
 
 export default router;
