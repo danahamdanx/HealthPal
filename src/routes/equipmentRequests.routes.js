59a71e6f7ae3cc3d05785e481327ad112f487053
danahamdanx <dana29454@gmail.com>
2025-11-29 19:47:58 +0200

    equipment request create routes

--- src/routes/equipmentRequests.routes.js
+++ src/routes/equipmentRequests.routes.js
@@ -7,10 +7,11 @@ import {
 } from "../controllers/equipmentRequests.controller.js";
 
 import { authorizeRoles } from '../middleware/authorize.middleware.js';
+import { authenticate } from "../middleware/authenticate.js";
 
 const router = express.Router();
 
-router.post("/", authorizeRoles, createEquipmentRequest);
+router.post("/",authenticate, authorizeRoles, createEquipmentRequest);
 router.post("/:id/claim", authorizeRoles, claimEquipmentRequest);
 router.patch("/:id/status", authorizeRoles, updateEquipmentRequestStatus);
 
