26723a39b4d38e84ca04ce958c4bfc47c3e461b4
danahamdanx <dana29454@gmail.com>
2025-12-03 19:27:40 +0200

    createHealthWorkshop controller

--- src/controllers/workshops&webinars.controller.js
+++ src/controllers/workshops&webinars.controller.js
@@ -7,3 +7,30 @@ export const getHealthWorkshops = async (req, res) => {
     res.status(500).json({ error: "Error fetching workshops" });
   }
 };
+
+export const createHealthWorkshop = async (req, res) => {
+  try {
+    const {
+      title, description, instructor, mode, location,
+      start_time, end_time, max_attendees
+    } = req.body;
+
+    const [result] = await db.query(`
+      INSERT INTO HealthWorkshops 
+      (title, description, instructor, mode, location, start_time, end_time, max_attendees)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    `, [
+      title, description, instructor,
+      mode || "online",
+      location || null,
+      start_time, end_time, max_attendees || null
+    ]);
+
+    const [workshop] = await db.query(`SELECT * FROM HealthWorkshops WHERE workshop_id=?`, [result.insertId]);
+
+    res.status(201).json(workshop[0]);
+
+  } catch {
+    res.status(500).json({ error: "Error creating workshop" });
+  }
+};
