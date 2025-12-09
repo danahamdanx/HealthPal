fc3d15d7037ae0f9f7ed8359a730495f1777c720
danahamdanx <dana29454@gmail.com>
2025-12-09 19:51:02 +0200

    reset and forget routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,5 +11,7 @@ router.post('/login', login);
 
 router.post('/logout', logout);
 
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 export default router;
