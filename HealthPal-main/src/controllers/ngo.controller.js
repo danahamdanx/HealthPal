957b05759d9176b813219caa44ac314617f685e4
Dana Hamdan <145384079+danahamdanx@users.noreply.github.com>
2025-12-10 19:16:47 +0200

    Delete HealthPal-main directory

--- HealthPal-main/src/controllers/ngo.controller.js
+++
@@ -1,9 +0,0 @@
-import { createCrudController } from './crud.controller.js';
-
-export const {
-  getAll: getAllNgos,
-  getById: getNgoById,
-  create: createNgo,
-  update: updateNgo,
-  delete: deleteNgo
-} = createCrudController('Ngos', 'ngo_id', ['name', 'registration_number', 'address', 'verified','phone','email','mission']);
